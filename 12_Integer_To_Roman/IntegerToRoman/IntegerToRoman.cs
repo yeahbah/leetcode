24c4b44d49ac4c3bc3b5f5e297c27cfe84d0aabf
Arnold Diaz <yeahbah@outlook.com>
2021-10-05 11:56:42 -0600

    Integer to roman numeral

---
+++ 12_Integer_To_Roman/IntegerToRoman/IntegerToRoman.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntegerToRoman
+{
+    public class Solution
+    {
+        public string IntToRoman(int num)
+        {
+            if (num < 0 || num > 3999) return "";
+
+            var symbols = new Dictionary<int, string>
+            {
+                { 1000, "M" },
+                { 900, "CM" },
+                { 500, "D" },
+                { 400, "CD" },
+                { 100, "C" },
+                { 90, "XC" },
+                { 50, "L" },
+                { 40, "XL" },
+                { 10, "X" },
+                { 9, "IX" },
+                { 5, "V" },
+                { 4, "IV" },
+                { 1, "I" },
+            };
+
+            var result = "";
+            while (num > 0)
+            {                
+                foreach (var symbol in symbols)
+                {
+                    decimal temp = num / symbol.Key;
+                    if (Math.Truncate(temp) > 0)
+                    {
+                        num -= symbol.Key;
+                        result += symbol.Value;
+                        break;
+                    }                    
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 12_Integer_To_Roman/IntegerToRoman/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntegerToRoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            //Console.WriteLine(IntegerToRoman.ConvertIntToRoman(3000));
+            //Console.WriteLine(IntegerToRoman.ConvertIntToRoman(1));
+            //Console.WriteLine(IntegerToRoman.ConvertIntToRoman(2));
+            //Console.WriteLine(IntegerToRoman.ConvertIntToRoman(10));
+            //Console.WriteLine(IntegerToRoman.ConvertIntToRoman(50));
+            //Console.WriteLine(IntegerToRoman.ConvertIntToRoman(100));
+            //Console.WriteLine(IntegerToRoman.ConvertIntToRoman(500));
+            Console.WriteLine(solution.IntToRoman(4));
+            Console.WriteLine(solution.IntToRoman(40));
+            Console.WriteLine(solution.IntToRoman(400));
+            Console.WriteLine(solution.IntToRoman(9));
+            Console.WriteLine(solution.IntToRoman(90));
+            Console.WriteLine(solution.IntToRoman(900));
+            Console.WriteLine(solution.IntToRoman(1994));
+        }
+    }
+}
